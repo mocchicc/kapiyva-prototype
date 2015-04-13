546b0976f92ba6d9a011bbff414d9d0b3038f1ce
Hiroyuki Tsuruda <mocchicc@me.com>
2015-04-13 11:45:05 +0900

    article#new action added.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,20 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		redirect_to articles_path
+	end
+
+	# Private関数で制限
+	private
+		def article_params
+				params[:article].permit(:title)
+		end
+
 end
